b1bc29658ec0ba1297a6e2ff7339e52274d710f6
Joshua Wise <joshua@joshuawise.com>
2022-06-06 04:01:28 -0400

    add notes on PWM and GPIOs

--- patches/api.h
+++ patches/api.h
@@ -9,6 +9,8 @@ extern void (*ui_modal_and_wait)(const char *s);
 extern void *(*malloc_annotated)(const char *file, const char *func, int line, int sz);
 extern void (*free)(void *p);
 extern void (*file_write)(const char *name, void *p, int len);
+extern int (*GPPFilLoad)(const char *name, void **retp, int *outsz);
+extern int (*sprintf)(char *dest, const char *s, ...);
 extern overlay_hnd *ui_modal_overlay;
 
 #endif
